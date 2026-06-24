6c483df294b506f5e1192271245f72eefc932716
Pramila Tamang <pramilatmg.np@gmail.com>
2026-06-25 00:48:41 +0545

    Few changes

--- RESTAURANT/urls.py
+++ RESTAURANT/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path("accounts/logout/",auth_views.LogoutView.as_view(),name="logout"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
